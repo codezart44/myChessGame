10a1e3bcdfbd796571786a21ce0f3acd44806885
Oskar Edén Wallberg <oswa.public@gmail.com>
2022-05-03 20:19:21 +0200

    commit test

--- Chessboard.py
+++ Chessboard.py
@@ -2,5 +2,7 @@
 
 class Chessboard:
     def __init__(self):
-        self.FEN = ""
-
+        self.FEN = "rhbqkbhr/--------/--------/--------/--------/RHBQKBHR"
+        self.board = [[0 for _ in range(8)] for _ in range(8)]
+        self.ctmm = "w"
+        self.castling_rights = [["q", "k"], ["Q", "K"]]
